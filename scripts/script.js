667fe70047c4f5d45a68f77937666040fde7f54f
jigoo <165612433+jisu0212@users.noreply.github.com>
2024-07-11 23:39:18 +0900

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const webS = new Swiper('.web .swiper',{
+    autoplay:{delay:1000},
+    loop:true,
+    navigation:{
+        nextEl:'.web .swiper-button-next',
+        prevEl:'.web .swiper-button-prev',
+    }
+})
