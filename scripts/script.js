22b884bd8a15f8842f010f4446e44cc9aee636e9
jigoo <165612433+jisu0212@users.noreply.github.com>
2024-07-12 18:08:28 +0900

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,11 @@ const webS = new Swiper('.web .swiper',{
         prevEl:'.web .swiper-button-prev',
     }
 })
+const landingS = new Swiper('.landing .swiper',{
+    autoplay:{delay:1000},
+    loop:true,
+    navigation:{
+        nextEl:'.web .swiper-button-next',
+        prevEl:'.web .swiper-button-prev',
+    }
+})
